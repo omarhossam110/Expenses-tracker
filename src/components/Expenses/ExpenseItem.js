bb576e4609b325dfbb0f3096a890eee8a6e7770d
omarhossam110 <m.omar.hossam@gmail.com>
2023-01-17 15:46:37 +0200

    add <li> for semantic purposess

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,32 +1,29 @@
-
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-
 function ExpenseItem(props) {
-
   //React Hook
   // const [title, setTitle] = useState(props.title);
-  
+
   // const clickHandler = () => {
   //     setTitle('Updated!!');
   //   console.log(title);
   // };
 
-
   return (
-  
-    <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
 
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      {/* <button onClick={clickHandler}>Change Title!</button> */}
-    </Card>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change Title!</button> */}
+      </Card>
+    </li>
   );
 }
 
