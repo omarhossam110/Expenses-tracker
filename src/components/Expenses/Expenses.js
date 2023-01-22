29716e09fcba3a827cf3412a3b5ea1a2beaa0adc
omarhossam110 <m.omar.hossam@gmail.com>
2023-01-22 15:56:52 +0200

    Added the Chart to the Expenses component and pass the values to it

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,8 @@ function Expenses(props) {
         defaultSelectedValue={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+
+      <ExpensesChart expenses={filteredExpenses}/>
 
       {/* {filteredExpenses.length === 0 && <p>No Expenses Found!</p> }
       {filteredExpenses.length > 0 && 
